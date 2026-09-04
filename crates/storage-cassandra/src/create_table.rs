84dd7e6c2e903045f6fea3e482edbfa9a5a1f2f0
Joel Shepherd <shepherd@amazon.com>
2026-09-04 20:09:47 +0000

    fix:fixing merge from main by plumbing table_throughput_mode through storage_cassandra

--- crates/storage-cassandra/src/create_table.rs
+++ crates/storage-cassandra/src/create_table.rs
@@ -519,6 +519,7 @@ impl CassandraEngine {
                 last_decrease_date_time: None,
             },
             billing_mode_summary,
+            table_throughput_mode_summary: None,
             global_secondary_indexes: gsis,
             local_secondary_indexes: lsis,
             stream_specification: input.stream_specification,
--- crates/storage-cassandra/src/table_helpers.rs
+++ crates/storage-cassandra/src/table_helpers.rs
@@ -250,6 +250,7 @@ impl CassandraEngine {
                 last_decrease_date_time: None,
             },
             billing_mode_summary,
+            table_throughput_mode_summary: None,
             global_secondary_indexes: if gsis.is_empty() { None } else { Some(gsis) },
             local_secondary_indexes: if lsis.is_empty() { None } else { Some(lsis) },
             stream_specification,
